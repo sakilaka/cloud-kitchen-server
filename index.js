27b4ecdf4f70799666d57115220be698fab42ff0
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-11-09 11:40:19 +0600

    jwt done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,11 +19,40 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@kamrul.iyiyxhu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+
+        return res.status(401).send({ errrr: 'unauthorized' })
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+
+        if (err) {
+            return res.status(401).sned({ errr: 'unauthorized' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
 async function run() {
 
     try {
         const serviceCollection = client.db('cloudKitchen').collection('services');
         const reviewCollection = client.db('cloudKitchen').collection('reviews');
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
         app.get('/services-limit', async (req, res) => {
             const query = {};
@@ -59,7 +90,36 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/reviews', async (req, res) => {
+        app.put('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const updateReview = req.body;
+            // console.log(updateReview);
+            const option = { upsert: true };
+            const updatedReview = {
+                $set: {
+                    foodID: updateReview.foodID,
+                    foodName: updateReview.foodName,
+                    email: updateReview.email,
+                    name: updateReview.name,
+                    photoURL: updateReview.photoURL,
+                    rating: updateReview.rating,
+                    message: updateReview.message
+                }
+            }
+            const result = await reviewCollection.updateOne(filter, updatedReview, option);
+            res.send(result);
+        })
+
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            console.log(req.headers.authorization);
+            // const decoded = req.decoded;
+            // console.log('decode', decoded);
+
+            // if (decoded.email !== req.query.email) {
+            //     return res.status(403).send({ errror: 'unauthorized' })
+            // }
+
             const queryEmail = req.query.email;
             let query = {};
             if (req.query.email) {
@@ -67,7 +127,8 @@ async function run() {
                     email: queryEmail
                 }
             }
-            const cursor = reviewCollection.find(query);
+            const cursor = reviewCollection.find(query).sort({ _id: -1 });
+            // const cursor = reviewCollection.find(query, sort);
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
